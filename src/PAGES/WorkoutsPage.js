fa6c16e07533b279b9a51be22d808ec7c9810939
Bogdyapple14 <bogdybogdan34@gmail.com>
2021-06-26 12:38:21 +0300

    Done : Workout Card

--- src/PAGES/WorkoutsPage.js
+++ src/PAGES/WorkoutsPage.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import WorkoutCard from "../COMPONENTS/WorkoutCard";
+
 const WorkoutsPage = () => {
+  const [state, setstate] = useState(initialState);
+
   return (
     <div id="page">
-      <div>SUGGESTED PROGRAMS</div>
+      <div className="w-full text-white">
+        <div className="mb-16">
+          <h2 className="text-4xl font-medium text-center mt-16">
+            SUGGESTED <span className="text-red-500">PROGRAMS</span>
+          </h2>
+          <h3 className="text-2xl text-center mt-3 font-medium text-gray-300">
+            Grow Big, Grow Strong
+          </h3>
+        </div>
+        <WorkoutCard />
+      </div>
     </div>
   );
 };
